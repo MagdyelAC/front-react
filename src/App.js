0ee1624bb19e0b90d61d6e419af1c7d7a06771cd
MagdyelAC <MagdyelAguilarCid@gmail.com>
2023-03-28 10:12:02 -0600

    Update my-app

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Magdyel Aguilar Cid <code>28/03/2023</code> 
         </p>
         <a
           className="App-link"
